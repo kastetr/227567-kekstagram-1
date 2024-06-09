d135b75ee6ee1350dc7aac26e2498c6f45f8e306
kastet <kostenckoroman.wotkastet1@gmail.com>
2024-06-09 17:43:00 +0200

    Добавляет вызов написанных функций

--- js/functions.js
+++ js/functions.js
@@ -6,6 +6,15 @@ const isPalindrome = (str) => {
   return cleanedStr === [...cleanedStr].reverse().join('');
 };
 
+// Строка является палиндромом
+isPalindrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPalindrome('ДовОд'); // true
+// Это не палиндром
+isPalindrome('Кекс'); // false
+// Это палиндром
+isPalindrome('Лёша на полке клопа нашёл '); // true
+
 const extractDigits = (input) => {
   // Преобразуем входные данные в строку, если они не являются строкой
   const str = typeof input === 'string' ? input : input.toString();
@@ -24,6 +33,12 @@ const extractDigits = (input) => {
   return number;
 };
 
+extractDigits('2023 год'); // 2023
+extractDigits('ECMAScript 2022'); // 2022
+extractDigits('1 кефир, 0.5 батона'); // 105
+extractDigits('агент 007'); // 7
+extractDigits('а я томат'); // NaN
+
 const padStart = (str, minLength, padString) => {
   // Проверяем, нужно ли дополнять строку
   if (str.length >= minLength) {
@@ -39,5 +54,27 @@ const padStart = (str, minLength, padString) => {
   return paddedString;
 };
 
+// Добавочный символ использован один раз
+padStart('1', 2, '0'); // '01'
+
+// Добавочный символ использован три раза
+padStart('1', 4, '0'); // '0001'
+
+// Добавочные символы обрезаны с конца
+padStart('q', 4, 'werty'); // 'werq'
+
+// Добавочные символы использованы полтора раза
+padStart('q', 4, 'we'); // 'wweq'
+
+// Добавочные символы не использованы, исходная строка не изменена
+padStart('qwerty', 4, '0'); // 'qwerty'
+
 //Кекстаграм
 const checkStringLength = (str, maxLength) => str.length <= maxLength;
+
+// Cтрока короче 20 символов
+checkStringLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkStringLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkStringLength('проверяемая строка', 10); // false
